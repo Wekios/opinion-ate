698caccc386d5842eb134867fac595ed3ca8b0bc
Veljko Blagojevic <veljkod.blagojevic@gmail.com>
2021-05-03 21:34:55 +0200

    Load restaurants upon first rendering RestaurantList

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,3 +1,13 @@
-export function RestaurantList() {
+import { useEffect } from 'react';
+
+export interface RestaurantListProps {
+  loadRestaurants: () => void;
+}
+
+export function RestaurantList({ loadRestaurants }: RestaurantListProps) {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+
   return <div>RestaurantList</div>;
 }
